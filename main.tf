fc16031143ee7a585cd9819854081eb67dcbd1a1
PRATIK JADHAV <89682851+pratik-haha@users.noreply.github.com>
2025-09-07 22:59:03 +0530

    adding updated to main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "first_instance" {
+  ami           = "ami-0fd3ac4abb734302a" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+  subnet_id = "subnet-0cfa05aebd2e89c3b"
+  }
