6c0e2640e22866ce344f67f7b5628d61a80c7fce
PRATIK JADHAV <89682851+pratik-haha@users.noreply.github.com>
2025-09-07 23:29:21 +0530

    adding correct instance type

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ provider "aws" {
 
 resource "aws_instance" "first_instance" {
   ami           = "ami-0fd3ac4abb734302a" # Amazon Linux 2 AMI
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   subnet_id = "subnet-0cfa05aebd2e89c3b"
   }
